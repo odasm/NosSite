6613ae579958458a08e4425811a6896521cb386c
ShidoEPVP <cravenvids1@web.de>
2016-09-10 18:54:01 +0200

    Add Error 404 Page

---
+++ 404.php
@@ -0,0 +1,38 @@
+
+<!doctype html>
+<html> 
+    <head>
+        <meta charset='utf-8'/>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'/>
+        <title>404 Not Found</title>
+        <link rel='stylesheet' href='//gf1.geo.gfsrv.net/cdnf9/ac8ed886ee14591a830586b83d3c21.css'/>
+        <link rel='stylesheet' type='text/css' href='//gf1.geo.gfsrv.net/cdn66/5ec42d576fd0d7bf3468c1ac556306.css' />
+        <link rel='icon' href='//gf3.geo.gfsrv.net/cdn2b/62605064e3da09ffbfa6f086a17eba.ico' type='image/x-icon' />
+        <link href='http://fonts.googleapis.com/css?family=Leckerli+One' rel='stylesheet' type='text/css' />
+    </head>
+    <body id='bg404'>
+        <div id='wrapper404'>
+            <div id='mainContent404'>
+                <div id='bobble404'> 
+                    <h1>404</h1>
+                    <p>Seite nicht gefunden!</p> 
+                </div>
+                <div id='buschi404'> </div>
+            </div>
+            <div id='bottomWrapper404'>
+                <div id='box1'>
+                    <h1>Nichts gefunden?</h1>
+                    <p>Kehre zur Startseite zur&uuml;ck!</p>
+                    <a class='button_404' href='/index.php'>
+                        <span> </span>Zur Startseite                    </a>
+                        </form>
+                    </div>
+                <div class='clear'></div>
+            </div>
+        </div>
+
+    </body>
+</html>;
+
+
+
